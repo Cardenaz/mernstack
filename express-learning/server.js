6bd4f0d95f73f1ebc815c741193d7cb555e64556
Cardenaz <william@cardenas.se>
2021-09-02 12:55:53 +0200

    break

--- express-learning/server.js
+++ express-learning/server.js
@@ -2,11 +2,15 @@ const express = require('express')
 const path = require('path');
 const app = express(); 
 const port = 3000; 
-
 const publicDirectoryPath = path.join(__dirname, 'public');
 app.set('view engine', 'hbs');
 app.use(express.static(publicDirectoryPath)); 
-
+app.get('', (req, res) => {
+    res.render('index', {
+        title: "Express Learning", 
+        name: "William Cardenas"
+    }); 
+});
 app.listen(port, () => {
     console.log(`server is running on port ${port}`); 
 
