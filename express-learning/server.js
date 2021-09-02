d301741806e3391abf9e153346622c9b73490ceb
Cardenaz <william@cardenas.se>
2021-09-02 12:46:05 +0200

    s

---
+++ express-learning/server.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const path = require('path');
+const app = express(); 
+const port = 3000; 
+
+const publicDirectoryPath = path.join(__dirname, 'public');
+app.use(express.static(publicDirectoryPath)); 
+
+app.listen(port, () => {
+    console.log(`server is running on port ${port}`); 
+
+})
